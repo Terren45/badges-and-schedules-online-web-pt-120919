d2aed4814bd914d7f8999d5d6bd5821e3f032fd2
Terren Culberson <culbersonterren@yahoo.com>
2019-12-17 03:42:07 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,7 +13,7 @@ end
 
 def assign_rooms(speakers)
   speakers.map.with_index do |name, index|
-    "Hello, #{name}! You'll be assigned to room #{index}!"
+    "Hello, #{name}! You'll be assigned to room #{index + 1}!"
    end
 end
   
