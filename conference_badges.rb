50bc7bc2e7c3a2b75fdee71356bb0d763b8ba550
Terren Culberson <culbersonterren@yahoo.com>
2019-12-17 02:52:25 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,12 +3,7 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-def batch_badge_creator ["Edsger", "Ada", "Charles", "Alan"]
-   assign_room.each do |room|
-  puts "Hello, #{name}!" 
-  puts "You'll be assiged to #{room}!"
-  return "Hello, _____!" 
-   return "You'll be assigned to room _____!"
-  counter += 1
- end
+def batch_badge_creator(names)
+  new_array[]
+    name.each do |name| 
 end 
