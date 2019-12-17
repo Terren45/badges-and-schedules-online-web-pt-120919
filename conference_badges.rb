e9bd6938c4a7cd58f9c78d030589881e76240b22
Terren Culberson <culbersonterren@yahoo.com>
2019-12-17 02:59:28 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,7 +6,7 @@ end
 def batch_badge_creator(names)
   new_array[]
     names.each do |name|
-      new_array.push(badge_maker(name))
+     new_array.push(badge_maker(name))
     end
     return new_array
 end 
